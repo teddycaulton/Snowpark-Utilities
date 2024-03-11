7fe2fe79a13807bb4bd8a594ee2fdced5d0e5a50
Teddy Caulton <118858063+teddycaultonhakkoda@users.noreply.github.com>
2024-03-11 14:31:25 -0700

    Update snowpark_utilities.py

--- snowpark_utilities/snowpark_utilities.py
+++ snowpark_utilities/snowpark_utilities.py
@@ -62,3 +62,66 @@ class snowpark_utilities:
     def execute_sql_pandas(session, command):
         sql_command = session.sql(command)
         return sql_command.to_pandas()
+    
+    def snoflake2snowflakevalidation(self, session_source, session_target, database):
+        results_db = pd.DataFrame()
+        databases = []
+        schemas = []
+        tables = []
+        source_count = []
+        target_count = []
+        source_db = self.execute_sql(session_source, f"SELECT DATABASE_NAME FROM SNOWFLAKE.INFORMATION_SCHEMA.DATABASES WHERE DATABASE_NAME='{database}'")
+        target_db = self.execute_sql(session_target, f"SELECT DATABASE_NAME FROM SNOWFLAKE.INFORMATION_SCHEMA.DATABASES WHERE DATABASE_NAME='{database}'")
+        if source_db!=target_db:
+            logging.error(f"Database {database} could not be found in either the source or target account.  please check spelling and the existence of the database in both accounts")
+
+        schemas_source = pd.DataFrame(self.execute_sql(session_source, f"SHOW SCHEMAS IN DATABASE {database}"))
+        for schema in schemas_source["name"]:
+            schema_target = pd.DataFrame(self.execute_sql(session_target, f"SHOW SCHEMAS LIKE '{schema}'"))
+            tables_source = pd.DataFrame(self.execute_sql(session_source, f"SHOW TABLES IN SCHEMA {database}.{schema}"))
+            if tables_source.empty:
+                continue
+            
+            if schema_target["name"][0] == schema and schema_target["database_name"][0] == database:
+                for table in tables_source["name"]:
+                    try:
+                        if table.isupper():
+                            source_row_count = self.execute_sql_pandas(session_source, f"SELECT COUNT(*) FROM {database}.{schema}.{table}")
+                            source_row_count = source_row_count["COUNT(*)"][0]
+                            target_row_count = self.execute_sql_pandas(session_target, f"SELECT COUNT(*) FROM {database}.{schema}.{table}")
+                            target_row_count = target_row_count["COUNT(*)"][0]
+                        else:
+                            source_row_count = self.execute_sql_pandas(session_source, f'SELECT COUNT(*) FROM {database}.{schema}."{table}"')
+                            source_row_count = source_row_count["COUNT(*)"][0]
+                            target_row_count = self.execute_sql_pandas(session_target, f'SELECT COUNT(*) FROM {database}.{schema}."{table}"')
+                            target_row_count = target_row_count["COUNT(*)"][0]
+                        if source_row_count == target_row_count:
+                            databases.append(database)
+                            schemas.append(schema)
+                            tables.append(table)
+                            source_count.append(source_row_count)
+                            target_count.append(target_row_count)
+                        elif source_row_count != target_row_count:
+                            databases.append(database)
+                            schemas.append(schema)
+                            tables.append(table)
+                            source_count.append(source_row_count)
+                            target_count.append(target_row_count)
+                    except Exception as e:
+                        source_row_count = "NA"
+                        target_row_count = "NA"
+                        databases.append(database)
+                        schemas.append(schema)
+                        tables.append(table)
+                        source_count.append(source_row_count)
+                        target_count.append(target_row_count)
+            else:
+                print(f"Schema '{schema}' does not exist in the target table")
+        
+        results_db["database"] = databases
+        results_db["schema"] = schemas
+        results_db["table"] = tables
+        results_db["source_count"] = source_count
+        results_db["target_count"] = target_count
+
+        return results_db
